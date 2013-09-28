89aeef0f1b433896612eaebc131626ebcbe21e70
Ben Webb <bjwebb67@googlemail.com>
2013-09-28 15:49:52 +0100

    Rework entity functions to allow antigrav jumping

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -2,6 +2,132 @@
 a player entity
 -------------------------------- */
 game.PlayerEntity = me.ObjectEntity.extend({
+                // Overload this so antigravity works properly
+                computeVelocity : function(vel) {
+
+			// apply gravity (if any)
+			if (this.gravity) {
+				// apply a constant gravity (if not on a ladder)
+				vel.y += !this.onladder?(this.gravity * me.timer.tick):0;
+
+				// check if falling / jumping
+                                if (this.gravity > 0)
+                                    this.falling = (vel.y > 0);
+                                else
+                                    this.falling = (vel.y < 0);
+				this.jumping = this.falling?false:this.jumping;
+			}
+
+			// apply friction
+			if (this.friction.x)
+				vel.x = me.utils.applyFriction(vel.x,this.friction.x);
+			if (this.friction.y)
+				vel.y = me.utils.applyFriction(vel.y,this.friction.y);
+
+			// cap velocity
+			if (vel.y !== 0)
+				vel.y = vel.y.clamp(-this.maxVel.y,this.maxVel.y);
+			if (vel.x !== 0)
+				vel.x = vel.x.clamp(-this.maxVel.x,this.maxVel.x);
+		},
+		updateMovement : function() {
+
+			this.computeVelocity(this.vel);
+
+			// Adjust position only on collidable object
+			var collision;
+			if (this.collidable) {
+				// check for collision
+				collision = this.collisionMap.checkCollision(this.collisionBox, this.vel);
+
+				// update some flags
+				this.onslope  = collision.yprop.isSlope || collision.xprop.isSlope;
+				// clear the ladder flag
+				this.onladder = false;
+
+
+
+				// y collision
+				if (collision.y) {
+					// going down, collision with the floor
+					this.onladder = collision.yprop.isLadder || collision.yprop.isTopLadder;
+
+					if ((collision.y > 0 && this.gravity > 0) || (collision.y < 0 && this.gravity < 0)) {
+						if (collision.yprop.isSolid	|| 
+							(collision.yprop.isPlatform && (this.collisionBox.bottom - 1 <= collision.ytile.pos.y)) ||
+							(collision.yprop.isTopLadder && !this.disableTopLadderCollision)) {
+							// adjust position to the corresponding tile
+							this.pos.y = ~~this.pos.y;
+                                                        if (this.gravity > 0) {
+                                                            this.vel.y = (this.falling) ?collision.ytile.pos.y - this.collisionBox.bottom: 0 ;
+                                                        }else {
+                                                            // FIXME
+                                                            this.vel.y = 0;
+                                                            }
+							this.falling = false;
+						}
+						else if (collision.yprop.isSlope && !this.jumping) {
+							// we stop falling
+							this.checkSlope(collision.ytile, collision.yprop.isLeftSlope);
+							this.falling = false;
+						}
+						else if (collision.yprop.isBreakable) {
+							if  (this.canBreakTile) {
+								// remove the tile
+								me.game.currentLevel.clearTile(collision.ytile.col, collision.ytile.row);
+								if (this.onTileBreak)
+									this.onTileBreak();
+							}
+							else {
+								// adjust position to the corresponding tile
+								this.pos.y = ~~this.pos.y;
+								this.vel.y = (this.falling) ?collision.ytile.pos.y - this.collisionBox.bottom: 0;
+								this.falling = false;
+							}
+						}
+					}
+					// going up, collision with ceiling
+					else if ((collision.y < 0 && this.gravity > 0) || (collision.y > 0 && this.gravity < 0)) {
+						if (!collision.yprop.isPlatform	&& !collision.yprop.isLadder && !collision.yprop.isTopLadder) {
+							this.falling = true;
+							// cancel the y velocity
+							this.vel.y = 0;
+						}
+					}
+				}
+
+				// x collision
+				if (collision.x) {
+
+					this.onladder = collision.xprop.isLadder || collision.yprop.isTopLadder;
+
+					if (collision.xprop.isSlope && !this.jumping) {
+						this.checkSlope(collision.xtile, collision.xprop.isLeftSlope);
+						this.falling = false;
+					} else {
+						// can walk through the platform & ladder
+						if (!collision.xprop.isPlatform && !collision.xprop.isLadder && !collision.xprop.isTopLadder) {
+							if (collision.xprop.isBreakable	&& this.canBreakTile) {
+								// remove the tile
+								me.game.currentLevel.clearTile(collision.xtile.col, collision.xtile.row);
+								if (this.onTileBreak) {
+									this.onTileBreak();
+								}
+							} else {
+								this.vel.x = 0;
+							}
+						}
+					}
+				}
+			}
+
+			// update player position
+			this.pos.add(this.vel);
+
+			// returns the collision "vector"
+			return collision;
+
+		},
  
     /* -----
  
@@ -28,7 +154,7 @@ game.PlayerEntity = me.ObjectEntity.extend({
         this.renderable.setCurrentAnimation("walk");
 
         // set the display to follow our position on both axis
-        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+        //me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
  
     },
  
@@ -41,12 +167,12 @@ game.PlayerEntity = me.ObjectEntity.extend({
  
         if (me.input.isKeyPressed('left')) {
             // flip the sprite on horizontal axis
-            this.flipX(true);
+            // this.flipX(true);
             // update the entity velocity
             this.vel.x -= this.accel.x * me.timer.tick;
         } else if (me.input.isKeyPressed('right')) {
             // unflip the sprite
-            this.flipX(false);
+            // this.flipX(false);
             // update the entity velocity
             this.vel.x += this.accel.x * me.timer.tick;
         } else {
@@ -54,10 +180,15 @@ game.PlayerEntity = me.ObjectEntity.extend({
         }
         if (me.input.isKeyPressed('jump')) {
             // make sure we are not already jumping or falling
+            console.log(this.jumping, this.falling);
             if (!this.jumping && !this.falling) {
                 // set current vel to the maximum defined value
                 // gravity will then do the rest
-                this.vel.y = -this.maxVel.y * me.timer.tick;
+                if (this.gravity > 0)
+                    this.vel.y = -this.maxVel.y * me.timer.tick;
+                else 
+                    this.vel.y = +this.maxVel.y * me.timer.tick;
+                console.log(this.vel.y);
                 // set the jumping flag
                 this.jumping = true;
             }
